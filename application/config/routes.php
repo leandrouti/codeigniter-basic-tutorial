db9ebfde1309be1dd740d7258ec1e096f1e702e3
Leandro T. Utiyama <lutiyama@gmail.com>
2020-03-29 09:51:14 +0900

    [add] news

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,5 +53,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $route['404_override'] = '';
 // $route['translate_uri_dashes'] = FALSE;
 
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
